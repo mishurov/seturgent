9c18aee1893584d7ecedb9d93903f7a1ec66dcf5
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2019-12-08 07:59:26 +0300

    Check if the window has the mail tab

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
+/* globals browser */
+
 function getAttention() {
-  browser.windows.getLastFocused({windowTypes: ['normal']}).then(recentWindow => {
-    if (recentWindow) {
+  const getInfo = {populate: true, windowTypes: ['normal']};
+  // should be getAll yet it throws an exception (bug) if a popup window is focused
+  browser.windows.getLastFocused(getInfo).then(recentWindow => {
+    if (recentWindow && recentWindow.tabs.some(t => t.mailTab)) {
       browser.windows.update(recentWindow.id, {drawAttention: true});
     }
   });
