1b7fcc4feb7d4186a58868aa10cbab06540c660c
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2019-12-08 20:20:08 +0300

    Use MailServices and convertMessage

--- background.js
+++ background.js
@@ -1,13 +1,24 @@
 /* globals browser */
 
+const excludedFolders = [
+  'drafts',
+  'sent',
+  'trash',
+  'templates',
+  'archives',
+  'junk',
+  'outbox',
+];
+
+browser.seturgent.onMessageArrived.addListener(function(messageHeader) {
+  if (!excludedFolders.includes(messageHeader.folder.type))
+    getAttention();
+});
+
 function getAttention() {
   browser.tabs.query({mailTab: true}).then(tabs => {
-    if (tabs.length) {
+    if (tabs.length)
       browser.windows.update(tabs[0].windowId, {drawAttention: true});
-    }
   });
 }
 
-browser.seturgent.onMessage.addListener(function() {
-  getAttention();
-});
--- implementation.js
+++ implementation.js
@@ -1,19 +1,21 @@
+/* globals ChromeUtils */
 var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
-var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var { MailServices } = ChromeUtils.import("resource:///modules/MailServices.jsm");
 
 
+// eslint-disable-next-line no-unused-vars
 var seturgent = class extends ExtensionCommon.ExtensionAPI {
   getAPI(context) {
     return {
       seturgent: {
-        onMessage: new ExtensionCommon.EventManager({
+        onMessageArrived: new ExtensionCommon.EventManager({
           context,
-          name: "seturgent.onMessage",
+          name: "seturgent.onMessageArrived",
           register(fire) {
-            function callback(event) {
-              return fire.async();
+            function callback(event, aMsgHdr) {
+              const messageHeader = context.extension.messageManager.convert(aMsgHdr);
+              return fire.async(messageHeader);
             }
-
             messageListener.add(callback);
             return function() {
               messageListener.remove(callback);
@@ -30,36 +32,12 @@ var messageListener = new class extends ExtensionCommon.EventEmitter {
   constructor() {
     super();
     this.callbackCount = 0;
-    this.notificationService = Cc["@mozilla.org/messenger/msgnotificationservice;1"]
-      .getService(Ci.nsIMsgFolderNotificationService);
-
-    var that = this;
+    this.notificationService = MailServices.mfn;
     this.msgListener = {
       msgAdded: function(aMsgHdr) {
-        that.processNewMail(aMsgHdr);
+        messageListener.emit("message-arrived", aMsgHdr);
       }
     };
-  }
-
-  processNewMail(aMsgHdr) {
-    if (!aMsgHdr.isRead && this.needsAttention(aMsgHdr.folder)) {
-      messageListener.emit("message-arrived");
-    }
-  }
-
-  needsAttention(folder) {
-    var flag = Ci.nsMsgMessageFlags;
-    if (folder.isSpecialFolder(flag.Drafts,1) ||
-        folder.isSpecialFolder(flag.SentMail,1) ||
-        folder.isSpecialFolder(flag.Templates,1) ||
-        folder.isSpecialFolder(flag.Archive,1) ||
-        folder.isSpecialFolder(flag.Trash,1) ||
-        folder.isSpecialFolder(flag.Queue,1) ||
-        folder.isSpecialFolder(flag.Junk,1)) {
-      return false;
-    } else {
-      return true;
-    }
   }
 
   add(callback) {
